b6e067b4e90118d7b92bc527571970f58aa69ad0
Michael Morfessis <mkmorfess@yahoo.com>
2018-03-02 18:48:43 -0600

    Receiving response from spoonacular

--- server/controller/yummlyAPI.js
+++ server/controller/yummlyAPI.js
@@ -9,8 +9,8 @@ const yumListURL = "https://api.yummly.com/v1/api/recipes" + yumAppId + yumKey +
 
 
 
-// const spoon = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=true&url=";
-// const recipeKey = "BEAGFpnJzNmshCwrMP5I5LTVCo3qp1L9ydsjsnkqXNYsWX08Dx";
+const spoon = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=true&url=";
+const recipeKey = "BEAGFpnJzNmshCwrMP5I5LTVCo3qp1L9ydsjsnkqXNYsWX08Dx";
 
 
 // Replace "chicken" in yummly request with userSearch variable once route has been identified for a search"
@@ -22,31 +22,31 @@ router.get("/search", function(req, res){
         // console.log("Body:", body);
         res.json(JSON.parse(body));
     })
-    // request({"url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/products/classify",
-    //          "headers": {
-    //             "X-Mashape-Key": "BEAGFpnJzNmshCwrMP5I5LTVCo3qp1L9ydsjsnkqXNYsWX08Dx",
-    //             "Content-Type": "application/json",
-    //             "Accept": "application/json"
-    //         },
-    //         "params": {
-    //             "ingredientList": "3 oz pork shoulder",
-    //             "title": "Pork roast with green beans"
-    //         }
-    // }, function(error, resp, data){
-    //     console.log("Error:", error);
-    //     console.log("Status Code:", resp && resp.statusCode);
-    //     console.log("Body:", data);
-    // })
 })
 //Gets a single recipe
 router.get("/search/:recipe_id", function(req, res){
     recipeId = req.params.recipe_id
-    const yumRecURL = "http://api.yummly.com/v1/api/recipe/" + recipeId + yumAppId + yumKey
+    console.log(req.params.recipe_id)
+    let yumRecURL = "http://api.yummly.com/v1/api/recipe/" + recipeId + yumAppId + yumKey
     request(yumRecURL, function(err, response, body){
         console.log("Error:", err);
         console.log("Status Code:", response && response.statusCode);
         console.log("Body:", body);
-        res.json(JSON.parse(body));
+        // res.json(JSON.parse(body));
+    })
+
+    request({
+        "url": spoon + encodeURI(yumRecURL),
+        "headers": {
+            "X-Mashape-Key": recipeKey,
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    }, function (error, resp, data) {
+        console.log("Error:", error);
+        console.log("Status Code:", resp && resp.statusCode);
+        console.log("Body:", data);
+        res.json(JSON.parse(data))
     })
 })
 
