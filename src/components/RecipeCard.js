55927e84692e10c3385de191ab8e7dc171691631
Michael Morfessis <mkmorfess@yahoo.com>
2018-03-13 19:33:14 -0600

    Front End: Updated with Modal for recipes on search page

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,17 +1,52 @@
 import React from 'react';
-
-
+import RecipeModal from './RecipeModal';
+import API from '../utils/api';
+import Modal from 'react-modal';
 
 class RecipeCard extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      modalIsOpen: false,
+      recipe: {}
+    }
   }
-  wrapperStyles ={
+
+  customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      color: 'black'
+    }
+  };
+
+  wrapperStyles = {
     backgroundImage: 'url(' + this.props.recipe.imageUrlBySize[90] + ')',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center'
   }
+
+  onHandleRecipePreview = id => {
+    this.setState({ modalIsOpen: true });
+    API.getDetailRecipe(id).then(response => {
+      console.log(response)
+      this.setState({  recipe: response.data });
+    })
+  }
+  
+  afterOpenModal =() => {
+    // references are now sync'd and can be accessed.
+  }
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  }
+
   render(){
     return(
       <div className="example-2 card">
@@ -28,7 +63,23 @@ class RecipeCard extends React.Component{
             <div className="content">
               <span className="author">Recipe Source Name</span>
               <h1 className="title"><a href="#">{this.props.recipe.recipe_name}</a></h1>
-              <a href="#" className="button__card">Preview Recipe</a>
+              <a onClick={() => this.onHandleRecipePreview(this.props.recipe.recipe_id)} className="button__card">Preview Recipe</a>
+              <Modal isOpen={this.state.modalIsOpen} onAfterOpen={this.afterOpenModal} onRequestClose={this.closeModal} ariaHideApp={false} style={this.customStyles} contentLabel="Example Modal">
+                <div>
+                  {this.state.recipe.name === undefined ? <img src="./images/loader.gif"/> : <div>
+                    <h2>{this.state.recipe.name}</h2>
+                    <img src={this.state.recipe.image} />
+                    <div>Servings: {this.state.recipe.numberOfServings}</div>
+                    <div>Total Time: {this.state.recipe.totalTime}</div>
+                    <div>Source URL: {this.state.recipe.source.sourceRecipeUrl}</div>
+                    <div>Ingredients: {this.state.recipe.ingredientLines}</div> 
+                  </div>}
+                  
+                  <button onClick={this.closeModal}>close</button>
+                </div>  
+        
+              </Modal>
+              
             </div>
           </div>
         </div>
