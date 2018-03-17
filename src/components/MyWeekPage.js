25705a60952934adacba6a56373fec34fd84084d
Michael Morfessis <mkmorfess@yahoo.com>
2018-03-17 14:56:17 -0600

    My Week Card working

--- src/components/MyWeekPage.js
+++ src/components/MyWeekPage.js
@@ -2,24 +2,67 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { saveUser } from '../actions/user';
 import MyWeekCard from './MyWeekCard';
+import { toast } from 'react-toastify';
 
 
 export class MyWeekPage extends React.Component {
 
-    onHandleClick =(e) => {
-        alert(e.target.value);
+    
+    onHandleGroceryList = (recipe) => {
+
+        let inGrocery = this.props.user.grocery_list.some(
+            item => item.id === recipe.id);
+
+        const {
+            favorites,
+            user_id,
+            recent_searches,
+            my_week,
+            grocery_list,
+            _id
+        } = this.props.user;
+
+        const newList = {
+            id: recipe.id,
+            name: recipe.name,
+            ingredients: recipe.ingredientLines,
+            servings: recipe.numberOfServings
+        }
+
+        let filteredList = [];
+
+        // grocery_list.push(newList)
+        if (!inGrocery) {
+            toast.info(`Added ${newList.name} to your Grocery List!`);
+            grocery_list.push(newList);
+        }
+        else {
+            toast.info(`Updated your Grocery List!`);
+            filteredList = grocery_list.filter(grocery => grocery.id != newList.id);
+        }
+
+        const updatedUser = {
+            favorites,
+            user_id,
+            recent_searches,
+            my_week,
+            grocery_list: inGrocery ? filteredList : grocery_list,
+            _id
+        }
+
+        this.props.saveUser(updatedUser);
     }
 
     render(){
         return (
             <div> 
-                <MyWeekCard day={'monday'} dayMeals={this.props.user.my_week.monday}/>
-                <MyWeekCard day={'tuesday'} dayMeals={this.props.user.my_week.tuesday} />   
-                <MyWeekCard day={'wednesday'} dayMeals={this.props.user.my_week.wednesday} />   
-                <MyWeekCard day={'thursday'} dayMeals={this.props.user.my_week.thursday} />   
-                <MyWeekCard day={'friday'} dayMeals={this.props.user.my_week.friday} />   
-                <MyWeekCard day={'saturday'} dayMeals={this.props.user.my_week.saturday} />   
-                <MyWeekCard day={'sunday'} dayMeals={this.props.user.my_week.sunday} />       
+                <MyWeekCard day={'monday'} dayMeals={this.props.user.my_week.monday} onHandleToGrocery={this.onHandleGroceryList} grocery_list={this.props.user.grocery_list}/>
+                <MyWeekCard day={'tuesday'} dayMeals={this.props.user.my_week.tuesday} onHandleToGrocery={this.onHandleGroceryList}  grocery_list={this.props.user.grocery_list}/>   
+                <MyWeekCard day={'wednesday'} dayMeals={this.props.user.my_week.wednesday} onHandleToGrocery={this.onHandleGroceryList} grocery_list={this.props.user.grocery_list}/>   
+                <MyWeekCard day={'thursday'} dayMeals={this.props.user.my_week.thursday} onHandleToGrocery={this.onHandleGroceryList} grocery_list={this.props.user.grocery_list}/>   
+                <MyWeekCard day={'friday'} dayMeals={this.props.user.my_week.friday} onHandleToGrocery={this.onHandleGroceryList} grocery_list={this.props.user.grocery_list}/>   
+                <MyWeekCard day={'saturday'} dayMeals={this.props.user.my_week.saturday} onHandleToGrocery={this.onHandleGroceryList} grocery_list={this.props.user.grocery_list}/>   
+                <MyWeekCard day={'sunday'} dayMeals={this.props.user.my_week.sunday} onHandleToGrocery={this.onHandleGroceryList} grocery_list={this.props.user.grocery_list}/>
             </div>
         );
     }
