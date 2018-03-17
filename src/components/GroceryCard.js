1b908f1bf01e2e635f724baa900a01a641038afe
jarishaw <jarishaw@gmail.com>
2018-03-16 20:48:20 -0500

    front end: partial styling of grocerycard comp

--- src/components/GroceryCard.js
+++ src/components/GroceryCard.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 const GroceryCard = (props) => (
     <form id="todo-list">
-            <h1 class="grocery=title"> Recipe: {props.grocery.name}</h1>
+            <h1 class="grocery=title"> {props.grocery.name}</h1>
             <p>Serving Size: {props.grocery.servings} People</p>
             
             <span class="todo-wrap">
-                {props.grocery.ingredients.length > 0 ? props.grocery.ingredients.map((ingredient, i) => <label class="todo" key={i}>{ingredient}</label>) : <li>Sorry no ingredients found</li>}
+            {props.grocery.ingredients.length > 0 ? props.grocery.ingredients.map((ingredient, i) => <label class="todo" key={i}>{ingredient}</label>) : <li>Sorry no ingredients found</li>}
             </span>
         
     </form>
