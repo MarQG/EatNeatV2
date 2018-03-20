a1458e79c7fd29dab2b48ad32972faedd1bc8ea5
Ferenc Gutierrez <ferenc.g87@gmail.com>
2018-03-20 17:37:57 -0500

    Production: Added Facebook Login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -9,6 +9,7 @@ export const LoginPage = ({ startLogin, startFB }) => (
             <img className="box_layout__logo" src="./images/eat_neat_logo_wht.png" alt="EAT NEAT"/>
             <h1 className="box-layout__title">Welcome to EatNeat.</h1>
             <button className="button button--google" onClick={startLogin}> <span className="fa fa-google"></span> Sign In with Google</button>
+            <button className="button button--facebook" onClick={startFB}> <span className="fa fa-facebook"></span> Sign In with Facebook</button>
         </div>
     </div>
 );
